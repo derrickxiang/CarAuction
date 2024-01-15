c62c68867b31bcbce102c1803a328659e51af9db
Derrick <derrick.xiang@gmail.com>
2024-01-15 13:10:15 +0800

    add contracts

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,5 +1,7 @@
+using MassTransit;
 using MongoDB.Driver;
 using MongoDB.Entities;
+using SearchService.Consumers;
 using SearchService.Data;
 using SearchService.Models;
 
@@ -8,6 +10,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddMassTransit(x => 
+{
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
+
+    x.UsingRabbitMq((context, cfg) => 
+    {
+        cfg.ConfigureEndpoints(context);
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
